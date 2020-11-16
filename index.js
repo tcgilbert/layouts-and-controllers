d83119510f4643deaee5ee952d3ddec6bff1ca2a
Thomas Gilbert <tcgilbert94@gmail.com>
2020-11-16 15:27:56 -0500

    setup layout

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //import stuff
 const express = require('express');
 const app = express();
+const ejsLayouts = require('express-ejs-layouts');
+
 
 app.set('view engine', 'ejs');
-
+app.use(ejsLayouts);
 
 //create our routes
 app.get('/', (req, res) => {
