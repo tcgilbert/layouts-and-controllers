06ad396d0b5f39a142f191f1d86c053069a40259
Thomas Gilbert <tcgilbert94@gmail.com>
2020-11-16 16:14:35 -0500

    add more views

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/faves/animals', (req, res) => {
+    const animals = ['peacock', 'unicorns', 'dogs'];
+    res.render('faveAnimals', { animals });
+});
+
+app.get('/hates/animals', (req, res) => {
+    const animals = ['cockroaches', 'chihuahuas', 'centipedes'];
+    res.render('hatedAnimals', { animals });
+});
+
+app.get('/faves/food', (req, res) => {
+    const foods = ['pizza', 'sushi', 'bbq', 'burritos'];
+    res.render('faveFood', { foods });
+});
+
+app.get('/hates/food', (req, res) => {
+    const foods = ['honeydew', 'oatmeal', 'cottage cheese'];
+    res.render('hatedFood', { foods });
+});
+
 //app.listen
 app.listen(8000, () => {
     console.log('server started');
